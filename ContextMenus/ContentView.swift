592a11806bb84dea56a9d4b081e00050d1ac1e78
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-01-14 10:05:08 +0200

    screen added

--- ContextMenus/ContentView.swift
+++ ContextMenus/ContentView.swift
@@ -11,6 +11,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
+            .contextMenu {
+                Button(action: {
+                    print("Copy button pressed")
+                }) {
+                    Text("Copy")
+                    Image(systemName: "doc.on.doc")
+                }
+        }
     }
 }
 
